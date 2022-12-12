b0eb25e825f5c0994dcde8fa86da23225efe985c
BreakerSneaker <brann1802@gmail.com>
2022-12-12 09:46:06 +0200

    index

---
+++ index.js
@@ -0,0 +1,7 @@
+var button = document.querySelector('#my-button');
+
+// Attach the event listener
+button.addEventListener('click', function myButton() {
+    alert("Hello! I am an alert box!!");
+  myFunction();
+});
