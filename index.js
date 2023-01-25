07c9b8426c2173ef6697a1ed9c3a02c284d68259
BreakerSneaker <brann1802@gmail.com>
2023-01-25 10:55:01 +0200

    index

--- index.js
+++ index.js
@@ -292,11 +292,11 @@ const semana = 51;
     d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
     // Set to nearest Thursday: current date + 4 - current day number
     // Make Sunday's day number 7
-    d.setUTCDate(d.getUTCDate() - 0 - (d.getUTCDay()||6));
+    d.setUTCDate(d.getUTCDate() - 1 - (d.getUTCDay()||6));
     // Get first day of year
     var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
     // Calculate full weeks to nearest Thursday
-    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 0)/7);
+    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 0)/6);
     // Return array of year and week number
     return [d.getUTCFullYear(), weekNo];
 }
